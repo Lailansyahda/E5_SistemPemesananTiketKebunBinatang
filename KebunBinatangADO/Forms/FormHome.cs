39c7accd708043864379698e64df31befb33a96f
Lailansyahda <syahdaazalia123@gmail.com>
2026-04-15 14:57:28 +0700

    menambahkan koneksi database pada form home

--- KebunBinatangADO/Forms/FormHome.cs
+++ KebunBinatangADO/Forms/FormHome.cs
@@ -13,6 +13,9 @@ namespace KebunBinatangADO.Forms
 {
     public partial class FormHome: Form
     {
+        string connString = "Data Source=LAPTOP-2V9KUAS1\\LAILANSYAHDA; Initial Catalog=DBKebunBinatangADO; Integrated Security=True";
+        SqlConnection conn;
+
         public FormHome()
         {
             InitializeComponent();
