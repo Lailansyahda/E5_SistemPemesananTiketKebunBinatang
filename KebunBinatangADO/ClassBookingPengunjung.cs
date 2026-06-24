13a8e44c868cb4872016ea99a5a1b1611b7d7ac0
Lailansyahda <syahdaazalia123@gmail.com>
2026-06-24 18:26:42 +0700

    membuat class booking pengunjung

---
+++ KebunBinatangADO/ClassBookingPengunjung.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KebunBinatangADO
+{
+    public class ClassBookingPengunjung
+    {
+        public string DetailTiket { get; set; }
+
+        public string TotalHarga { get; set; }
+
+        public string KodeBooking { get; set; }
+
+        public string StatusPembayaran { get; set; }
+
+        public DateTime TanggalKunjungan { get; set; }
+    }
+}
