e73825ffd0116bd7928b8c05350e3182bf0da8af
Lailansyahda <syahdaazalia123@gmail.com>
2026-06-24 13:32:32 +0700

    membuat class laporan booking

---
+++ KebunBinatangADO/ClassLaporanBooking.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KebunBinatangADO
+{
+    class ClassLaporanBooking
+    {
+        public string KodeBooking { get; set; }
+
+        public string Nama { get; set; }
+
+        public string NoHp { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime TanggalKunjungan { get; set; }
+
+        public string TotalHarga { get; set; }
+
+        public string StatusPembayaran { get; set; }
+    }
+}
